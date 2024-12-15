857c88c670953b253bba9a98ca67be83a5bd22bc
Santiago Lo Coco <slococo@itba.edu.ar>
2024-12-15 20:47:19 +0100

    Use only neo4j in graph_preparation

--- src/part1/graph_preparation.py
+++ src/part1/graph_preparation.py
@@ -1,73 +1,45 @@
-import requests
-
 from src.lib.config import NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD, NEO4J_DATABASE, OMDB_API_KEY, OMDB_API_URL
 from src.lib.neo4j_connector import Neo4jConnector
-from src.lib.utils import extract_runtime
 
 
-def get_movie_details_from_omdb(imdb_id):
-    """Fetch the movie information from OMDb API using IMDb ID."""
-    url = f"{OMDB_API_URL}/?i={imdb_id}&apikey={OMDB_API_KEY}"
-    response = requests.get(url).json()
-    return response
-
-
-def fix_all_movie_ids(db):
-    """Fix all movie IMDb IDs that are missing the 'tt' prefix."""
-    query = """
-    MATCH (m:Movie)
-    WHERE m.imdbId IS NOT NULL
-    SET m.imdbId = CASE
-        WHEN substring(m.imdbId, 0, 2) <> 'tt' THEN 'tt' + m.imdbId
-        ELSE m.imdbId
-    END
-    """
-    db.execute_query(query)
-    print("Fixed IMDb IDs for all movies.")
-
-
-def update_movies_without_imdb_rating(db, limit):
-    """Find movies without IMDb ratings and update them using the OMDb API."""
+def update_movies_with_imdb_rating(db, limit):
     query = """
     MATCH (m:Movie)
     WHERE m.imdbRating IS NULL AND m.imdbId IS NOT NULL
-    RETURN m.imdbId AS imdbId
-    LIMIT $limit
+    WITH m, 
+         CASE 
+             WHEN NOT m.imdbId STARTS WITH 'tt' THEN 'tt' + m.imdbId 
+             ELSE m.imdbId 
+         END AS fixedImdbId LIMIT $limit
+    SET m.imdbId = fixedImdbId
+    WITH m, fixedImdbId AS imdbId
+    CALL apoc.load.json($omdbApiUrl + "/?i=" + imdbId + "&apikey=" + $apikey) 
+    YIELD value
+    SET m.imdbRating = value.imdbRating,
+        m.year = value.Year,
+        m.runtime = CASE 
+            WHEN value.Runtime IS NOT NULL AND value.Runtime <> "N/A" 
+            THEN toInteger(SPLIT(value.Runtime, " ")[0]) 
+            ELSE NULL 
+        END
+    RETURN m.title AS title, 
+           m.imdbId AS imdbId, 
+           m.imdbRating AS imdbRating, 
+           m.year AS year, 
+           m.runtime AS runtime
     """
-    movies, _, _ = db.execute_query(query, {"limit": limit})
-
-    for movie in movies:
-        imdb_id = movie['imdbId']
-
-        movie_details = get_movie_details_from_omdb(imdb_id)
-        print(movie_details)
-
-        if movie_details.get("Response") == "True":
-            query_update = """
-            MATCH (m:Movie {imdbId: $imdbId})
-            SET m.imdbId = $imdbId,
-                m.imdbRating = $imdbRating,
-                m.imdbVotes = $imdbVotes,
-                m.year = $year,
-                m.runtime = $runtime
-            """
-            db.execute_query(query_update, {
-                'imdbId': imdb_id,
-                'imdbRating': movie_details.get('imdbRating'),
-                'imdbVotes': movie_details.get('imdbVotes'),
-                'year': movie_details.get('Year'),
-                'runtime': extract_runtime(movie_details.get('Runtime'))
-            })
-            print(f"Updated movie {movie_details.get("Title")} with IMDb ID {imdb_id}")
-        else:
-            print(f"Failed to fetch details for IMDb ID {imdb_id}")
+    movies, _, _ = db.execute_query(query, {
+        "omdbApiUrl": OMDB_API_URL,
+        "apikey": OMDB_API_KEY,
+        "limit": limit
+    })
+    print(movies)
+    print("Movies updated with IMDb Rating")
 
 
 if __name__ == "__main__":
     db = Neo4jConnector(NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD, NEO4J_DATABASE)
 
-    fix_all_movie_ids(db)
-
-    update_movies_without_imdb_rating(db, 5000)
+    update_movies_with_imdb_rating(db, 10)
 
     db.close()
